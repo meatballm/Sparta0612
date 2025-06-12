c28036d3fa521422be8a09a130683caba9ee9798
KimYounghun <186786431+hkY333@users.noreply.github.com>
2025-06-12 20:52:47 +0900

    feat : 스크립터블 오브젝트로 스탯 생성
    Characters 스크립트에 스크립터블 오브젝트 상속받아 간단한 스탯 구현
    + PlayerMovement → PlayerController 이름 수정

--- Assets/Scripts/Younghun/Characters.cs
+++ Assets/Scripts/Younghun/Characters.cs
@@ -2,7 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Characters : MonoBehaviour
+
+[CreateAssetMenu(fileName = "New StatData", menuName = "Stats/Chracter Stats")]
+public class Characters : ScriptableObject
 {
-    
+    // 플레이어와 몬스터 공통 스탯
+    // 체력, 이동속도, 공격력
+    public int health;
+    public float moveSpeed;
+    public float attackDamage;
 }
