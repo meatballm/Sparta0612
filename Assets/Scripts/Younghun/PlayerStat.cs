189f0ccf09f8217f46e6536e8647b3c165e50371
KimYounghun <186786431+hkY333@users.noreply.github.com>
2025-06-13 00:41:51 +0900

    chore : InputSystem 패키지 설치
    + feat : cooldownDodge 선언 및 프로퍼티화

--- Assets/Scripts/Younghun/PlayerStat.cs
+++ Assets/Scripts/Younghun/PlayerStat.cs
@@ -1,9 +1,17 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerStat : MonoBehaviour
 {
+    [SerializeField] private bool canDodge;
+    [SerializeField] private float cooldownDodge;
+    public float CooldownDodge
+    {
+        get => cooldownDodge;
+        set => cooldownDodge = value;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
