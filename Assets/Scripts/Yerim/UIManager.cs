b7aa0d0178f2f82ce94e572ea4839ae2565d9e1c
han-yerim <eternity7236@naver.com>
2025-06-13 12:29:25 +0900

    add : 일시 정지 UI
    아직 구현은X 배치만

---
+++ Assets/Scripts/Yerim/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance {  get; private set; }
+
+    [Header("UI요소들")]
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject settings;
+    [SerializeField] private GameObject gameUI;
+    [SerializeField] private GameObject endPanel;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    public void MainMenu()
+    {
+        mainMenu.SetActive(true);
+        settings.SetActive(false);
+        gameUI.SetActive(false);
+        endPanel.SetActive(false);
+    }
+}
