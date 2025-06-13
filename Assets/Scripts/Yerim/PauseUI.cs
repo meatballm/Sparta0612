0e3028b7cb179f4e345daa2991cc787fb631c735
han-yerim <eternity7236@naver.com>
2025-06-13 16:48:05 +0900

    feat : 일시 정지 UI

---
+++ Assets/Scripts/Yerim/PauseUI.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseUI : MonoBehaviour
+{
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private GameObject outline;
+    [SerializeField] private Button pauseBtn;
+    [SerializeField] private Button continueBtn;
+    [SerializeField] private Button settingsBtn;
+    [SerializeField] private Button mainMenuBtn;
+
+    private void Start()
+    {
+        pausePanel.SetActive(false);
+        outline.SetActive(false);
+
+        // 버튼 연결
+        pauseBtn.onClick.AddListener(() => PanelOpen());
+        continueBtn.onClick.AddListener(OnContinue);
+        settingsBtn.onClick.AddListener(OnSettings);
+        mainMenuBtn.onClick.AddListener(OnMainMenu);
+    }
+
+    public void PanelOpen()
+    {
+        Time.timeScale = 0f; // 일시 정지
+        pausePanel.SetActive(true);
+        outline.SetActive(true);
+    }
+
+    public void PanelClose()
+    {
+        pausePanel.SetActive(false);
+        outline.SetActive(false);
+        Time.timeScale = 1f; // 게임 재개
+    }
+
+    private void OnContinue()
+    {
+        PanelClose();
+    }
+
+    private void OnSettings()
+    {
+
+    }
+
+    private void OnMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("StartScene"); // 씬 전환
+    }
+}
--- Assets/Scripts/Yerim/UIManager.cs
+++ Assets/Scripts/Yerim/UIManager.cs
@@ -6,23 +6,28 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager Instance {  get; private set; }
 
-    [Header("UI요소들")]
-    [SerializeField] private GameObject mainMenu;
-    [SerializeField] private GameObject settings;
-    [SerializeField] private GameObject gameUI;
-    [SerializeField] private GameObject endPanel;
+    [SerializeField] private PauseUI pauseUI;
+    public PauseUI Pause => pauseUI;
+
+    //[Header("UI요소들")]
+    //[SerializeField] private GameObject mainMenu;
+    //[SerializeField] private GameObject settings;
+    //[SerializeField] private GameObject gameUI;
+    //[SerializeField] private GameObject endPanel;
 
     private void Awake()
     {
-        if (Instance == null) Instance = this;
-        else Destroy(gameObject);
+        if(Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
     }
 
-    public void MainMenu()
-    {
-        mainMenu.SetActive(true);
-        settings.SetActive(false);
-        gameUI.SetActive(false);
-        endPanel.SetActive(false);
-    }
+    //public void MainMenu()
+    //{
+    //    mainMenu.SetActive(true);
+    //    settings.SetActive(false);
+    //    gameUI.SetActive(false);
+    //    endPanel.SetActive(false);
+    //}
 }
