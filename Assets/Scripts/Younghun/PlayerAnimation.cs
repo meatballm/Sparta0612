d1fc06b709097257f13383ec8c3af58714100914
KimYounghun <186786431+hkY333@users.noreply.github.com>
2025-06-15 02:19:10 +0900

    feat : Walk, Idle 애니메이션을 플레이어 움직임과 연동

--- Assets/Scripts/Younghun/PlayerAnimation.cs
+++ Assets/Scripts/Younghun/PlayerAnimation.cs
@@ -1,24 +1,63 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Animator))]
 public class PlayerAnimation : MonoBehaviour
 {
-    //public Animation 
-    [SerializeField] private PlayerInput playerinput;
+    [SerializeField] private PlayerInput playerInput; // Input System 참조
+    private Animator animator;
 
+    private Vector2 moveInput;
+    private Vector2 lastDirection;
 
-
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        animator = GetComponent<Animator>();
+
+        if (playerInput == null)
+        {
+            playerInput = GetComponent<PlayerInput>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // 현재 입력값 읽기 (Input System)
+        moveInput = playerInput.actions["Move"].ReadValue<Vector2>().normalized;
+
+        bool isMoving = moveInput.sqrMagnitude > 0.01f;
+
+        animator.SetBool("IsMoving", isMoving);
+        animator.SetFloat("MoveX", moveInput.x);
+        animator.SetFloat("MoveY", moveInput.y);
+
+        if (isMoving)
+        {
+            // 이동 중일 때 마지막 방향 갱신
+            lastDirection = moveInput;
+
+            animator.SetFloat("LastX", moveInput.x);
+            animator.SetFloat("LastY", moveInput.y);
+        }
+
+        else
+        {
+
+        }
     }
+
+    //// 방향 인덱스 정리 (옵션: Animator에서 DirectionIndex 사용 시)
+    //private int GetDirectionIndex(Vector2 dir)
+    //{
+    //    dir = dir.normalized;
+
+    //    if (dir.y > 0.5f && Mathf.Abs(dir.x) < 0.5f) return 0; // Up
+    //    if (dir.y < -0.5f && Mathf.Abs(dir.x) < 0.5f) return 1; // Down
+    //    if (dir.x < -0.5f && dir.y > 0.5f) return 2; // LeftUp
+    //    if (dir.x > 0.5f && dir.y > 0.5f) return 3; // RightUp
+    //    if (dir.x < -0.5f && dir.y < -0.5f) return 4; // LeftDown
+    //    if (dir.x > 0.5f && dir.y < -0.5f) return 5; // RightDown
+
+    //    return -1; // 입력 없음 (에러 처리용)
+    //}
 }
