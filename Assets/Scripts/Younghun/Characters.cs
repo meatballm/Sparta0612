4d906cec677156990cf5943c543532cbb7782a17
KimYounghun <186786431+hkY333@users.noreply.github.com>
2025-06-15 21:11:52 +0900

    feat : 무기가 캐릭터 절반 넘어가면 FlipY 활성화

--- Assets/Scripts/Younghun/Characters.cs
+++ Assets/Scripts/Younghun/Characters.cs
@@ -5,7 +5,7 @@ public class Character : MonoBehaviour
 {
     protected Rigidbody2D _rigidbody; // 이동을 위한 물리 컴포넌트
 
-    [SerializeField] private SpriteRenderer characterRenderer; // 좌우 반전을 위한 렌더러
+    [SerializeField] private SpriteRenderer weaponRenderer; // 좌우 반전을 위한 렌더러
     [SerializeField] private Transform weaponPivot; // 무기를 회전시킬 기준 위치
 
     protected Vector2 movementDirection = Vector2.zero; // 현재 이동 방향
@@ -67,15 +67,13 @@ public class Character : MonoBehaviour
     private void Rotate(Vector2 direction)
     {
         float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        //bool isLeft = Mathf.Abs(rotZ) > 90f;
-
-        //// 스프라이트 좌우 반전
-        //characterRenderer.flipX = isLeft;
+        bool isLeft = Mathf.Abs(rotZ) > 90f;
 
         if (weaponPivot != null)
         {
             // 무기 회전 처리
             weaponPivot.rotation = Quaternion.Euler(0, 0, rotZ);
+            weaponRenderer.flipY = isLeft;
         }
     }
 
