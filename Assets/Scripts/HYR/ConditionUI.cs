9729cddee52bb3b02114e813b8a5c8acb1a7b4ff
han-yerim <eternity7236@naver.com>
2025-06-16 07:05:28 +0900

    feat : 인벤토리, 쿨타임 추가
    하단 서브 인벤토리 슬롯, 재장전 쿨타임 등 게임 UI기능 추가

---
+++ Assets/Scripts/HYR/ConditionUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConditionUI : MonoBehaviour
+{
+    [SerializeField] private Image hpFill;
+    [SerializeField] private Image staminaFill;
+    [SerializeField] private GameObject reloadCooldownIcon;
+    [SerializeField] private Image reloadProgress;
+
+    public void SetHP(float ratio)
+    {
+        hpFill.fillAmount = Mathf.Clamp01(ratio);
+    }
+
+    public void SetStamina(float ratio)
+    {
+        staminaFill.fillAmount = Mathf.Clamp01(ratio);
+    }
+
+    public void ShowReloadCooldown(float duration)
+    {
+        reloadCooldownIcon.SetActive(true);
+        StartCoroutine(UpdateReloadBar(duration));
+    }
+
+    private IEnumerator UpdateReloadBar(float duration)
+    {
+        float time = 0f;
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            reloadProgress.fillAmount = time / duration;
+            yield return null;
+        }
+
+        reloadProgress.fillAmount = 1f;
+        yield return new WaitForSeconds(0.3f);
+        reloadCooldownIcon.SetActive(false);
+    }
+}
--- Assets/Scripts/HYR/GameUI.cs
+++ Assets/Scripts/HYR/GameUI.cs
@@ -7,20 +7,20 @@ public class GameUI : MonoBehaviour
 {
     [SerializeField] private Button pauseBtn;
 
+    [SerializeField] private ConditionUI conditionUI;
+    [SerializeField] private SubInventory subInventory;
+
+    public ConditionUI Condition => conditionUI;
+    public SubInventory SubInventory => subInventory;
+
     private void Start()
     {
         pauseBtn.onClick.AddListener(OnPause);
+        //Debug.Log("SubInventory 연결됨: " + (subInventory != null));
     }
 
     private void OnPause()
     {
-        if (UIManager.Instance?.Pause != null)
-        {
-            UIManager.Instance.Pause.OpenPanel();
-        }
-        else
-        {
-            Debug.LogWarning("PauseUI 참조가 null입니다!");
-        }
+        UIManager.Instance.Pause.OpenPanel();
     }
 }
