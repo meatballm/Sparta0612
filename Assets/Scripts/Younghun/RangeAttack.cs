258ac610c6c46a068970bded56d16482a92d050a
KimYounghun <186786431+hkY333@users.noreply.github.com>
2025-06-16 09:41:22 +0900

    feat : 무기별 스탯 ScriptableObject로 기본 구성

---
+++ Assets/Scripts/Younghun/RangeAttack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeaponType
+{
+    ARAssaultRifle = 0,
+    Shotgun,
+    SniperRifle
+}
+[CreateAssetMenu(fileName = "New WeaponData", menuName = "WeaponData")]
+public class RangeAttack : ScriptableObject
+{
+    public uint maxAmmo;
+    public float fireRate;
+    public uint bulletsPerShot;
+    public float reloadSpeed;
+    public uint damagePerShot;
+    public float spread;
+    public float bulletSpeed;
+    public ushort pierceCount;
+    public LayerMask target;
+}
