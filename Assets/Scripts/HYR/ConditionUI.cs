e76668833fc536f4dae1d70539e336fec6f6d838
han-yerim <eternity7236@naver.com>
2025-06-16 14:03:34 +0900

    add : NPC 대화 에셋 추가

--- Assets/Scripts/HYR/ConditionUI.cs
+++ Assets/Scripts/HYR/ConditionUI.cs
@@ -10,6 +10,8 @@ public class ConditionUI : MonoBehaviour
     [SerializeField] private GameObject reloadCooldownIcon;
     [SerializeField] private Image reloadProgress;
 
+    private Coroutine currentReloadRoutine;
+
     public void SetHP(float ratio)
     {
         hpFill.fillAmount = Mathf.Clamp01(ratio);
@@ -23,7 +25,23 @@ public class ConditionUI : MonoBehaviour
     public void ShowReloadCooldown(float duration)
     {
         reloadCooldownIcon.SetActive(true);
-        StartCoroutine(UpdateReloadBar(duration));
+
+        if (currentReloadRoutine != null)
+            StopCoroutine(currentReloadRoutine);
+
+        currentReloadRoutine = StartCoroutine(UpdateReloadBar(duration));
+    }
+
+    public void CancelReloadCooldown()
+    {
+        if (currentReloadRoutine != null)
+        {
+            StopCoroutine(currentReloadRoutine);
+            currentReloadRoutine = null;
+        }
+
+        reloadCooldownIcon.SetActive(false);
+        reloadProgress.fillAmount = 0f;
     }
 
     private IEnumerator UpdateReloadBar(float duration)
---
+++ Assets/Scripts/HYR/NPCInteraction.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCInteraction : MonoBehaviour
+{
+    [SerializeField] private GameObject outline;
+    [SerializeField] private GameObject talkIcon;
+
+    private bool isPlayerNearby = false;
+    private bool isTalking = false;
+
+    private void Start()
+    {
+        outline.SetActive(false);
+        talkIcon.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (isPlayerNearby && !isTalking && Input.GetKeyDown(KeyCode.F))
+        {
+            StartConversation();
+        }
+    }
+
+    private void StartConversation()
+    {
+        if (isTalking) return;
+        isTalking = true;
+
+        Debug.Log("대화중");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNearby = true;
+            outline.SetActive(true);
+            talkIcon.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNearby = false;
+            outline.SetActive(false);
+            talkIcon.SetActive(false);
+        }
+    }
+}
