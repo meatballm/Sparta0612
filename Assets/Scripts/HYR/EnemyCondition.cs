3bc947b66b144eeecfd190d1575f671c1de899eb
han-yerim <eternity7236@naver.com>
2025-06-17 20:02:42 +0900

    feat : 몬스터 체력바 구현단계

---
+++ Assets/Scripts/HYR/EnemyCondition.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyCondition : MonoBehaviour
+{
+    [SerializeField] private GameObject hpBarPrefab;
+    [SerializeField] private float offsetY = -1.0f; // 몬스터로부터 체력바 띄울 거리
+
+    private GameObject hpBarInstance;
+    private Image hpFillImage;
+    private Canvas hpBarCanvas;
+
+    private void Start()
+    {
+        // Canvas 하위 프리팹 인스턴스 생성
+        hpBarInstance = Instantiate(
+            hpBarPrefab,
+            Vector3.zero,
+            Quaternion.identity,
+            GameObject.Find("Canvas").transform
+        );
+
+        // fillAmount로 체력바 이미지 연결
+        hpFillImage = hpBarInstance.transform.Find("Bar/Fill_H").GetComponent<Image>();
+        if (hpFillImage == null)
+            Debug.LogError("Fill_H 이미지를 찾지 못했습니다!");
+
+        hpBarInstance.SetActive(false);
+
+        hpBarCanvas = hpBarInstance.GetComponentInParent<Canvas>();
+    }
+
+    // 체력바 갱신
+    public void UpdateHealthBar(int currentHP, int maxHP)
+    {
+        if (hpFillImage != null)
+        {
+            float ratio = (float)currentHP / maxHP;
+            hpFillImage.fillAmount = Mathf.Clamp01(ratio);
+
+            // 처음 데미지 입으면 체력바 활성화
+            if (!hpBarInstance.activeSelf)
+                hpBarInstance.SetActive(true);
+        }
+    }
+
+    // 체력바 제거
+    public void HideAndDestroyBar()
+    {
+        if (hpBarInstance != null)
+            Destroy(hpBarInstance);
+    }
+
+    private void LateUpdate()
+    {
+        // 항상 몬스터 아래쪽에 체력바가 따라가도록 이동
+        if (hpBarInstance != null && hpBarCanvas != null)
+        {
+            Vector3 worldPos = transform.position + new Vector3(0, offsetY, 0);
+            Vector3 screenPos = Camera.main.WorldToScreenPoint(worldPos);
+            hpBarInstance.transform.position = screenPos;
+        }
+    }
+}
