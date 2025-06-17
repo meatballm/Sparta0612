7e21e810802af61d32865a5e29f63895d046b0e3
meatballm <wh9555115@naver.com>
2025-06-17 11:51:41 +0900

    add : 포탈 추가

---
+++ Assets/Scripts/JSM/Portal.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    [Tooltip("이동할 씬 이름")]
+    public string nextSceneName;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
